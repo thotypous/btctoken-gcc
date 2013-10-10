3466504c942904d445f940ee28357c26aee43548
Paulo Matias <paulo.matias@usp.br>
2013-10-10 13:35:30 +0000

    Update header

--- pbkdf2.h
+++ pbkdf2.h
@@ -40,4 +40,13 @@ void hmac_sha256_init(HMAC_SHA256_CTX *, const void *, size_t);
 void hmac_sha256_update(HMAC_SHA256_CTX *, const void *, size_t);
 void hmac_sha256_final(unsigned char [32], HMAC_SHA256_CTX *);
 
+
+/**
+ * pbkdf2_sha256(passwd, passwdlen, salt, saltlen, c, buf, dkLen):
+ * Compute PBKDF2(passwd, salt, c, dkLen) using HMAC-scrypt_SHA256 as the PRF, and
+ * write the output to buf.  The value dkLen must be at most 32 * (2^32 - 1).
+ */
+void pbkdf2_sha256(const uint8_t *, size_t, const uint8_t *, size_t,
+    uint32_t, uint8_t *, size_t);
+
 #endif // PBKDF2_H
