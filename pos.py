7d07352be2e9f264472f8ba697ca9189c06fe8b6
Paulo Matias <paulo.matias@usp.br>
2013-10-11 14:42:48 +0000

    Implemented communication with the device

--- pos.py
+++ pos.py
@@ -1,18 +1,103 @@
-import time, thread, sys, socket, os
+import struct, time, thread, sys, socket, os
 from binascii import unhexlify, hexlify
 from decimal import Decimal
-import traceback
+import pyudev
 from electrum import Wallet, Interface, WalletVerifier, SimpleConfig, WalletSynchronizer, Transaction, bitcoin, util
 
 dest_addr = '1Ee7G1y5odBa93CTtfDgFxcxQHmiwcYRNw'
 tx_amount = Decimal('0.1')
 assert bitcoin.is_valid(dest_addr)
 
+def find_usbid(dev):
+    """Walk pyudev device parents until USB idVendor and idProduct
+       informations are found"""
+    ids = ['idVendor', 'idProduct']
+    while dev:
+        attr = dev.attributes
+        if False not in [x in attr for x in ids]:
+            return tuple([int(attr[x], 16) for x in ids])
+        dev = dev.parent
+
+def wait_dev(vendor, product, subsystem='hidraw'):
+    """Wait for a device with the supplied USB vendor and product IDs
+       to be attached and identified by a given subsystem.
+       Returns a pyudev device object."""
+    context = pyudev.Context()
+    monitor = pyudev.Monitor.from_netlink(context)
+    monitor.filter_by(subsystem)
+    monitor.start()
+    for dev in iter(monitor.poll, None):
+        if dev.action == 'add':
+            usbid = find_usbid(dev)
+            if not usbid:
+                print('Could not recognize USB ID for device %s' % dev.device_path)
+                continue
+            print('USB device %04x:%04x plugged' % usbid)
+            if usbid == (vendor, product):
+                print('USB ID matches the expected one')
+                return dev
+
+class BTCToken:
+    def __init__(self):
+        print 'waiting for the device to be plugged'
+        udev_dev = wait_dev(0xffff, 0x0bad)
+        self.dev = open(udev_dev.device_node, 'r+b', buffering=0)
+    def send(self, packet):
+        print 'send:', repr(packet)
+        self.dev.write('\x00' + packet.ljust(64, '\x00'))
+    def recv(self):
+        packet = self.dev.read(64)
+        print 'recv:', repr(packet)
+        return packet
+    def ask_pubkey(self):
+        self.send('BTCToken')
+        packet = self.recv()
+        assert(packet.startswith('Yes I am'))
+        size, = struct.unpack('<H', packet[8:10])
+        pubkey = ''
+        while size > 0:
+            packet = self.recv()
+            pubkey += packet[:min(len(packet), size)]
+            size -= len(packet)
+        return pubkey
+    def send_tx(self, tx):
+        self.send('RAWTXlen'+struct.pack('<H',len(tx)))
+        while len(tx):
+            self.send(tx[:64])
+            tx = tx[64:]
+    def recv_ok(self):
+        assert(self.recv().startswith('OK'))
+    def send_sameaddr(self, idx):
+        self.send('SameAddr'+struct.pack('<B',idx))
+    def send_thirdparty(self):
+        self.send('ThirdPty')
+    def send_merkle(self, merkle, pos):
+        for i,x in enumerate(merkle):
+            self.send('MerkleNode'+chr((pos >> i) & 1)+x.decode('hex')[::-1])
+        self.send('MerkleNode\xff')
+    def send_blocks(self, verifier, blk):
+        while True:
+            hdr = verifier.header_to_string(verifier.read_header(blk))
+            print 'blk #', blk, hdr
+            hdr = unhexlify(hdr)
+            blk += 1
+            self.send('Blk1'+hdr[:36])
+            self.send('Blk2'+hdr[36:])
+            recved = self.recv()
+            if recved.startswith('Trusted'):
+                break
+            assert(recved.startswith('MoreData'))
+    def recv_dbg(self, num_dbg):
+        for i in xrange(num_dbg):
+            print repr(self.recv())
+
+btctoken = BTCToken()
+orig_pubkey = btctoken.ask_pubkey()
+orig_addr = bitcoin.public_key_to_bc_address(orig_pubkey)
+print 'Costumer bitcoin address:', orig_addr
+
 interface = Interface({'server':'electrum.no-ip.org:50002:s'})
 interface.start()
-
-orig_pubkey = unhexlify('042d2a61ff964c566605a4a4e65df757b3136b1ca5e3166fa08d8e0397a3a196b75997387118c7df30235c4fa76f619168d274032470a24582320687b7202b032b')
-orig_addr = bitcoin.public_key_to_bc_address(orig_pubkey)
 
 wallet_config = SimpleConfig({
     'wallet_path': os.path.join('cache','%s.wallet'%orig_addr)
@@ -56,6 +141,8 @@ tx_amount = int(tx_amount*Decimal('1e8'))  # from BTC to Satoshi
 raw_tx = wallet.mktx([(dest_addr, tx_amount)], None, None, orig_addr, None)
 print 'raw tx:', raw_tx.raw
 print repr(raw_tx.deserialize())
+btctoken.send_tx(unhexlify(raw_tx.raw))
+btctoken.recv_ok()
 print '\n\n'
 
 for tx_in in raw_tx.inputs:
@@ -68,23 +155,38 @@ for tx_in in raw_tx.inputs:
         print 'input tx signed by same addr #',idx_same
         print 'raw tx:',tx.raw
         print repr(tx.deserialize())
-        print '\n\n'
+        btctoken.send_sameaddr(idx_same)
+        btctoken.send_tx(unhexlify(tx.raw))
+        btctoken.recv_ok()
     else:
         print 'input tx signed by third parties'
         print 'raw tx:',tx.raw
         print repr(tx.deserialize())
         tx_height, _ = verifier.get_txpos(tx_hash)
         res = interface.synchronous_get([ ('blockchain.transaction.get_merkle',[tx_hash,tx_height]) ])[0]
+        btctoken.send_thirdparty()
+        btctoken.send_tx(unhexlify(tx.raw))
         print 'sending merkle tree'
         print repr((res['merkle'], tx_hash, res['pos']))
+        btctoken.send_merkle(res['merkle'], res['pos'])
         print 'sending headers starting from blk %d' % tx_height
-        for i in xrange(6):
-            hdr = verifier.read_header(tx_height+i)
-            print repr(hdr)
-            print 'raw block hdr:', verifier.header_to_string(hdr)
-            
-        print '\n\n'
+        btctoken.send_blocks(verifier, tx_height)
+        btctoken.recv_ok()
+    print '\n\n'
+
+print 'debugging info'
+btctoken.recv_dbg(3)
+print '\n\n'
+
+for i in range(len(raw_tx.inputs)):
+    tx_for_sig = raw_tx.serialize( raw_tx.inputs, raw_tx.outputs, for_sig = i )
+    print 'tx_for_sig:', tx_for_sig
+    tx_for_sig_hash = bitcoin.Hash(unhexlify(tx_for_sig))
+    print 'tx_for_sig_hash:', hexlify(tx_for_sig_hash)
+print '\n\n'
 
 # get signatures from device
 print 'should now get', len(raw_tx.inputs), 'signatures'
+for i in xrange(len(raw_tx.inputs)):
+    print 'sig:', hexlify(btctoken.recv())
 # broadcast to network using wallet.sendtx(Transaction(hex_string))
